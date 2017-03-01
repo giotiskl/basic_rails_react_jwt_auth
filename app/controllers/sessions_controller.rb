cfee857700c46ce8f07208d4830dd8c0dc1b5306
Yiotis Kaltsikis <giotisgr@gmail.com>
2017-03-01 12:00:35 +0100

    initial commit, set up jwt auth for rails api

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,18 @@
+class SessionsController < ApplicationController
+  skip_before_action :authenticate
+
+  # login user, start new session
+  def create
+    user = User.find_by(email: auth_params[:email])
+    if user.authenticate(auth_params[:password])
+      jwt = Auth.issue({user: user.id})
+      render json: {jwt: jwt}
+    else
+    end
+  end
+
+  private
+  def auth_params
+    params.require(:auth).permit(:email, :password)
+  end
+end
